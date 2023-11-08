55b34cbdf832ba2482d55bdf003592d5fa646601
Jennifer Feenstra Arengard <jennifer.feenstra-arengard.appliedtechnology.se>
2023-11-08 16:20:45 +0100

    getting images from unsplash

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ console.log(pastSearches);
 const form = document.getElementById("form") as HTMLFormElement;
 const formInput = document.getElementById("search-form") as HTMLInputElement;
 const ul = document.getElementById("searchList") as HTMLUListElement;
+const next = document.getElementById("next") as HTMLElement;
+const prev = document.getElementById("prev") as HTMLElement;
+let resultStats = document.querySelector(".js-result-stats") as HTMLElement;
 
 if (localStorage["pastSearches"]) {
   pastSearches = JSON.parse(localStorage["pastSearches"]);
@@ -26,6 +29,7 @@ formInput.addEventListener("click", (e) => {
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  // handleSubmit(e);
   searchQuery = formInput.value;
   console.log(searchUnsplash(searchQuery));
   console.log(pastSearches);
@@ -51,9 +55,14 @@ form.addEventListener("submit", function (e) {
   }
   console.log("THE BITTER END", pastSearches);
   console.log("outside", itemFromStorage);
+  const inputValue = formInput.value; //formInput
+  searchQuery = inputValue.trim();
+  console.log(searchQuery);
+  fetchResults(searchQuery);
 });
 
 const addItemToDom = (item: string[]) => {
+  ul.innerHTML = ""; // empty the array
   console.log("tjotjopp", item);
   for (let i = 0; i < item.length; i++) {
     const li = document.createElement("li");
@@ -62,6 +71,14 @@ const addItemToDom = (item: string[]) => {
   }
 };
 
+// function handleSubmit(event: any) {
+//   event.preventDefault();
+//   const inputValue = formInput.value; //formInput
+//   searchQuery = inputValue.trim();
+//   console.log(searchQuery);
+//   fetchResults(searchQuery);
+// }
+
 async function searchUnsplash(searchQuery: string) {
   const endpoint = `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${accessKey}`;
   const response = await fetch(endpoint);
@@ -69,12 +86,13 @@ async function searchUnsplash(searchQuery: string) {
     throw Error(response.statusText);
   }
   const json = await response.json();
+  console.log(typeof json);
   return json;
 }
 
 //console.log(searchUnsplash(searchQuery));
 
-async function fetchResults(searchQuery) {
+async function fetchResults(searchQuery: string) {
   try {
     const results = await searchUnsplash(searchQuery);
     console.log(results);
@@ -85,16 +103,28 @@ async function fetchResults(searchQuery) {
   }
 }
 
+//We need to display;
+function displayResults(json: any) {
+  const searchResults = document.querySelector(
+    ".js-search-results"
+  ) as HTMLElement;
+  searchResults.textContent = "";
+  console.log(json);
+  json.results.forEach((result: any) => {
+    const url = result.urls.small;
+    const unsplashLink = result.links.html;
+    // const photographer = result.user.name;
+    // const photographerPage = result.user.links.html;
+    searchResults.insertAdjacentHTML(
+      "beforeend",
+      `<div>
+              <a href="${unsplashLink}" target="_blank">
+                  <div class="result-item" style="background-image: url(${url});"></div>
+              </a>
+          </div>`
+    );
+  });
+}
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   
-    <div>Picture1</div>
-    <div>Picture2</div>
-    <div>Picture3</div>
-    <div>Picture4</div>
-    <div>Picture5</div>
-    <div>Picture6</div>
-    <div>Picture7</div>
-    <div>Picture8</div>
-    <div>Picture9</div>
-  </div>
 `;
